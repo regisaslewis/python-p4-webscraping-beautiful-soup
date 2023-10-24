9350b35c9083f5818bfb82708c98d404f7ea66f2
RegisLewis <regisaslewis@gmail.com>
2023-10-24 15:55:23 -0700

    scraping HTML with Beautiful Soup

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,8 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, "html.parser")
+courses = doc.select(".heading-25-extrabold.color-black")[0]
+
+print(courses.attrs)
